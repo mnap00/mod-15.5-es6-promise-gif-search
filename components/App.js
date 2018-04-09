65f93ff098f1d468c1bcf84bf38269124f5649a8
mnap <learn@10g.pl>
2018-04-09 22:44:12 +0200

    Add initial state and update Gif props

--- components/App.js
+++ components/App.js
@@ -1,4 +1,11 @@
 App =  React.createClass({
+    getInitialState: function() {
+        return {
+            loading: false,
+            searchingText: '',
+            gif: {}
+        };
+    },
     render: function() {
         var styles = {
             margin: '0 auto',
@@ -7,10 +14,14 @@ App =  React.createClass({
         };
         return (
             <div style={styles}>
-                    <h1>GIF Search Machine</h1>
-                    <p>Find GIF on <a href='http://giphy.com'>giphy</a>. If you wanna search another gif - press enter.</p>
-                    <Search />
-                <Gif />
+                <h1>GIF Search Machine</h1>
+                <p>Find GIF on <a href='http://giphy.com'>giphy</a>. If you wanna search another gif - press enter.</p>
+                <Search />
+                <Gif
+                    loading={this.state.loading}
+                    url={this.state.gif.url}
+                    sourceUrl={this.state.gif.sourceUrl}
+                />
             </div>
         );
     }
