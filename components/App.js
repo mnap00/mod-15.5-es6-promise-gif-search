1d5a2fdf06d41fad5316834b5f72190807b1a3f8
mnap <learn@10g.pl>
2018-04-09 22:59:22 +0200

    Add handleSearch method

--- components/App.js
+++ components/App.js
@@ -6,6 +6,18 @@ App =  React.createClass({
             gif: {}
         };
     },
+    handleSearch: function(searchingText) {
+        this.setState({
+            loading: true
+        });
+        this.getGif(searchingText, function(gif) {
+            this.setState({
+                loading: false,
+                gif: gif,
+                searchingText: searchingText
+            });
+        }.bind(this));
+    },
     render: function() {
         var styles = {
             margin: '0 auto',
@@ -16,12 +28,14 @@ App =  React.createClass({
             <div style={styles}>
                 <h1>GIF Search Machine</h1>
                 <p>Find GIF on <a href='http://giphy.com'>giphy</a>. If you wanna search another gif - press enter.</p>
-                <Search />
+                <Search
+                    onSearch={this.handleSearch}
+                />
                 <Gif
                     loading={this.state.loading}
                     url={this.state.gif.url}
                     sourceUrl={this.state.gif.sourceUrl}
-                />
+               />
             </div>
         );
     }
