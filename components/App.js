4e4a2877804c16228c7b995f82fd1ae40a59b3b0
mnap00 <learn@10g.pl>
2018-04-10 12:35:27 +0200

    Add giphy API url portions

--- components/App.js
+++ components/App.js
@@ -1,4 +1,25 @@
+var GIPHY_API_URL = 'http://api.giphy.com/';
+var GIPHY_PUB_KEY = 'dc6zaTOxFJmzC';
 App =  React.createClass({
+    getGif: function(searchingText, callback) {
+        var url = GIPHY_API_URL +
+            'v1/gifs/random?api_key=' + GIPHY_PUB_KEY +
+            '&tag=' + searchingText;
+        console.log(url);
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText).data;
+                var gif = {
+                    url: data.fixed_width_downsampled_url,
+                    sourceUrl: data.url
+                };
+                callback(gif);
+            }
+        };
+        xhr.send();
+    },
     getInitialState: function() {
         return {
             loading: false,
